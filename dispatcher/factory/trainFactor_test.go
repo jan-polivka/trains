2b4b25b823d7adb20d5328106567d791ea805fc5
Jan Polivka <j.polivka5@gmail.com>
2023-04-09 09:13:01 +0000

    test update

--- dispatcher/factory/trainFactor_test.go
+++ dispatcher/factory/trainFactor_test.go
@@ -2,25 +2,18 @@ package factory
 
 import (
 	"dispatcher/types"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetTrainRegioSBahn(t *testing.T) {
-	var result, _ = GetTrain("RegioSBahn")
+	var result, _ = GetTrain(RegioSBahnType)
 	var expected = types.NewRegioSBahn()
 	assert.IsType(t, expected, result)
 }
 
 func Test_GetTrainStrassenBahn(t *testing.T) {
-	var result, _ = GetTrain("StrassenBahn")
-	fmt.Println(result)
-}
-
-func Test_GetTrainError(t *testing.T) {
-	var result, err = GetTrain("WrongTrain")
-	assert.Nil(t, result)
-	assert.Error(t, err)
+	// var result, _ = GetTrain("StrassenBahn")
+	// fmt.Println(result)
 }
