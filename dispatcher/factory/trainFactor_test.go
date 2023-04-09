c8bc74739fcc96fd7911a206399c8317c6f9841e
Jan Polivka <j.polivka5@gmail.com>
2023-04-09 09:16:43 +0000

    test update

--- dispatcher/factory/trainFactor_test.go
+++ dispatcher/factory/trainFactor_test.go
@@ -20,5 +20,7 @@ func Test_GetTrainStrassenBahn(t *testing.T) {
 }
 
 func Test_GetTrainWrongType(t *testing.T) {
-
+	var result, err = GetTrain(-1)
+	assert.Nil(t, result)
+	assert.Error(t, err)
 }
