75be85dd13a39193ad7269db641fb00644ab5670
Jan Polivka <j.polivka5@gmail.com>
2023-04-12 18:43:45 +0000

    working

--- dispatcher/main.go
+++ dispatcher/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	var opts []grpc.DialOption
+	var serverAddr = flag.String("addr", "localhost:8080", "The server address in the format of host:port")
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		fmt.Println("something went fucky wucky")
+	}
+	defer conn.Close()
 }
