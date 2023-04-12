dc6657191e569c3ddf4c877258f63f92c67ec717
Jan Polivka <j.polivka5@gmail.com>
2023-04-12 20:41:03 +0000

    working

--- dispatcher/main.go
+++ dispatcher/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 
-	pb "proto-api/protos"
-
 	"google.golang.org/grpc"
 )
 
@@ -13,11 +11,11 @@ func main() {
 	// var opts []grpc.DialOption
 	conn, err := grpc.Dial("localhost:8080")
 	if err != nil {
+		fmt.Println(conn)
 		fmt.Println("something went fucky wucky")
 	}
 	defer conn.Close()
-	client := pb.NewDispatchServiceClient(conn)
-	fmt.Println(client)
+	// client := pb.NewDispatchServiceClient(conn)
 
 	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	// defer cancel()
