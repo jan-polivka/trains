0fe49d398acec6594a0cb89df5541f110edd28b4
Jan Polivka <j.polivka5@gmail.com>
2023-04-13 18:11:36 +0000

    working

--- dispatcher/main.go
+++ dispatcher/main.go
@@ -9,11 +9,13 @@ import (
 	pb "proto-api/protos"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	var serverAddr = flag.String("addr", "localhost:8080", "The server address in the format of host:port")
-	conn, err := grpc.Dial(*serverAddr)
+	var dialOption = grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.Dial(*serverAddr, dialOption)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("something went fucky wucky")
