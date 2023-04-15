a764c5c1866fa503cee2afc476142e7039eba79c
Jan Polivka <j.polivka5@gmail.com>
2023-04-15 12:08:01 +0000

    working

--- dispatcher/sender/sender.go
+++ dispatcher/sender/sender.go
@@ -9,8 +9,6 @@ import (
 
 func DispatchTrains(client pb.DispatchServiceClient, ctx context.Context) int64 {
 
-	//test
-
 	resp, err := client.DispatchTrains(ctx, &pb.Trains{Train: []*pb.Train{{Name: "RegioSBahn", Drive: "ElectricDrive"}}})
 	if err != nil {
 		fmt.Println("the response is fucked")
